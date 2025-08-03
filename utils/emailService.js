577c0ab158f65de447beaa86d48fcbaa8fce718d
James <jamesasuelimen77@gmail.com>
2025-08-03 12:40:44 +0100

    booking finally completed

--- utils/emailService.js
+++ utils/emailService.js
@@ -3,6 +3,7 @@ const nodemailer = require("nodemailer");
 // Create reusable transporter
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
